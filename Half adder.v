62bc85bad37db12b7438b2fc1f57715575be4fac
Mahmoud Magdi <72949261+MahmouodMagdi@users.noreply.github.com>
2022-12-04 01:13:57 +0200

    Update Half adder.v

--- Half adder.v
+++ Half adder.v
@@ -11,33 +11,3 @@ assign Sum = A ^ B;
 assign Carry = A & B;
     
 endmodule
-
-// **************************************** Test Bench ************************************* //
-
-module half_adder_tb #( parameter DATA_WIDTH = 4 ) ();
-
-reg  [DATA_WIDTH - 1 : 0] A;
-reg  [DATA_WIDTH - 1 : 0] B;
-wire [DATA_WIDTH - 1 : 0] Sum;
-wire                      Carry;
-
-
-// Half Adder Instantaition
-half_adder UUT(A,B,Sum,Carry);
-
-
-// Stimulus
-initial begin
-    A = 4'b0110;
-    B = 4'b0001;
-
-    #20
-
-    A = 4'b1000;
-    B = 4'b0010;
-
-    #100 $finish; 
-end
-
-    
-endmodule
