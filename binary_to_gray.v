f3aca5579e14bd4f2c01b6d4e2938a8f587d24bb
Mahmoud Magdi <72949261+MahmouodMagdi@users.noreply.github.com>
2022-12-04 01:12:42 +0200

    Update binary_to_gray.v

--- binary_to_gray.v
+++ binary_to_gray.v
@@ -18,36 +18,3 @@ module binary_to_gray #(
          assign gray_out_data[i] = binary_in_data[i+1] ^ binary_in_data[i];
     end
 endmodule
-
-
-// **************************************** TEST BENCH *******************************************
-
-module bin_to_gry_tb #( parameter DATA_WIDTH = 16)();
-
-reg  [DATA_WIDTH - 1 : 0] binary_in_data;
-wire [DATA_WIDTH - 1 : 0] gray_out_data;
-
-
-// Unit Under Test Instantiation
-binary_to_gray (
-
- binary_in_data,
- gray_out_data
-    
-);
-
-
-// Stimulus Generation
-initial begin
-    binary_in_data = 16'b0010_1110_1001_0110;
-
-    #50
-    binary_in_data = 16'b1101_0010_1111_0011;
-
-    #50
-    binary_in_data = 16'b1101_0001_0110_1001;
-
-end
-    
-    
-endmodule
