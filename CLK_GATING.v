3fd515dfe5507f307a1c871116cfcb376f16f46f
Mahmoud Magdi <72949261+MahmouodMagdi@users.noreply.github.com>
2022-12-11 02:00:57 +0200

    Update CLK_GATING.v

--- CLK_GATING.v
+++ CLK_GATING.v
@@ -13,8 +13,9 @@ assign Gated_CLK = CLK & Sel;
 always @(posedge Gated_CLK)
 begin
 
-	q <= 1'b1;									// Here the input of the FF is not changed, So we gated the clk of the ff 
-												// to save more power
+	q <= 1'b1;									// As you can see, input of the flop is tied to a constant. Naturally, output will
+											// remain at this constant value at all the time irrespective of the clock applied at its
+input.
 end
 
 endmodule 
